dc88dc6994dcba735779f6a74850d00f0acbd0f0
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-19 17:06:47 +0300

    Add initial BST Constructors

---
+++ tests/test_bst.c
@@ -0,0 +1,19 @@
+#include <criterion/criterion.h>
+#include "bst.h"
+
+Test(test_bst, BST_Construct) {
+    BST* bst = BST_ConstructD();
+    BST* bst2 = BST_Construct(NULL);
+
+    cr_expect(bst != NULL);
+    cr_expect(bst2 != NULL);
+
+    cr_expect(bst->root == NULL);
+    cr_expect(bst2->root == NULL);
+
+    cr_expect(bst->count == 0);
+    cr_expect(bst2->count == 0);
+
+    BST_Free(bst);
+    BST_Free(bst2);
+}
