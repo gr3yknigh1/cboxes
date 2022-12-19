9edd204d1c178dc8a4b87df1389227cd79c0f4fb
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-19 16:11:47 +0300

    Add Node_Equals decl

--- include/node.h
+++ include/node.h
@@ -1,6 +1,7 @@
 #ifndef NODE_H
-#define NODE_H 
+#define NODE_H
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 typedef struct Node {
@@ -11,5 +12,7 @@ typedef struct Node {
 
 Node* Node_Construct(void* value, Node* next, size_t size);
 void Node_Free(Node* node);
+
+bool Node_Equals(const Node* node, const Node* other);
 
 #endif // NODE_H
