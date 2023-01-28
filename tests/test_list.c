ba1a2577090c874180420d89a28cd9936757224d
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-01-28 11:10:37 +0300

    Add tests for cs_List creation and basic push ops

--- tests/test_list.c
+++ tests/test_list.c
@@ -1,33 +1,98 @@
+#include <stdlib.h>
+#include <time.h>
 #include <criterion/criterion.h>
-#include <criterion/internal/assert.h>
 
 #include "cboxes/list.h"
+#include "cboxes/shallow.h"
 
-void setup(void) {}
+void setup(void) {
+    srand(time(NULL));
+}
+
 void teardown(void) {}
 
 TestSuite(test_list, .init = setup, .fini = teardown);
 
-// TODO: Check head and tail values
-Test(test_list, List_Basic) {
+int randInt(int min, int max) {
+    return min + rand() % (max + 1 - min);
+}
+
+int *generateArray(size_t length, int min, int max) {
+    int *array = malloc(sizeof(int) * length);
+    for (size_t i = 0; i < length; i++) {
+        array[i] = randInt(min, max);
+    }
+    return array;
+}
+
+Test(test_list, List_Creation_Full) {
+    cs_Type type = {
+        .isReference = false,
+        .size = sizeof(int),
+        .copy = cs_ShallowCopy,
+        .free = cs_ShallowFree,
+    };
+    cs_List *list = cs_List_New(type);
+
+    cr_assert(list != NULL);
+    cr_assert(list->head == NULL);
+    cr_assert(list->tail == NULL);
+    cr_assert(list->length == 0);
+    cr_assert(sizeof(*list) == 56);
+}
+
+Test(test_list, List_Creation_Default) {
     cs_List *list = cs_List_NewD(sizeof(int));
 
-    for (int i = 0; i < 9; i++) {
-        cs_List_PushBack(list, &i);
-    }
-
-    cs_LNode *cur = list->head;
-    u32 idx = 0;
-    while (cur != NULL) {
-        cr_assert(*(int *)cur->value == idx);
-
-        cur = cur->next;
-        idx++;
-    }
-
-    cs_List_PushBack(list, NULL);
-
-    int *x = NULL;
-    cr_assert(CS_LIST_GET(list, 1, &x) == cs_OK);
-    cr_assert(*x == 1);
+    cr_assert(list != NULL);
+    cr_assert(list->head == NULL);
+    cr_assert(list->tail == NULL);
+    cr_assert(list->length == 0);
+    cr_assert(sizeof(*list) == 56);
 }
+
+Test(test_list, List_Operations_PushBack) {
+    cs_List *list = cs_List_NewD(sizeof(int));
+
+    size_t sourceArrayLength = 10;
+    int *sourceArray = generateArray(sourceArrayLength, 0, 20);
+
+    for (size_t i = 0; i < sourceArrayLength; i++) {
+        cs_List_PushBack(list, sourceArray + i);
+    }
+
+    cr_assert(list->length == sourceArrayLength);
+
+    CS_LIST_FOREACH(list, int, i, storedValue, {
+        int *sourceValue = sourceArray + i;
+        cr_assert(sourceValue != storedValue);
+        cr_assert(*sourceValue == *storedValue);
+    });
+
+    cr_assert(list->head->prev == NULL);
+    cr_assert(list->tail->next == NULL);
+}
+
+Test(test_list, List_Operations_PushFront) {
+    cs_List *list = cs_List_NewD(sizeof(int));
+
+    size_t sourceArrayLength = 10;
+    int *sourceArray = generateArray(sourceArrayLength, 0, 20);
+
+    for (size_t i = 0; i < sourceArrayLength; i++) {
+        cs_List_PushFront(list, sourceArray + i);
+    }
+
+    cr_assert(list->length == sourceArrayLength);
+
+    CS_LIST_FOREACH(list, int, i, storedValue, {
+        int *sourceValue = sourceArray - i + sourceArrayLength - 1;
+        cr_assert(sourceValue != storedValue);
+        cr_assert(*sourceValue == *storedValue);
+    });
+
+    cr_assert(list->head->prev == NULL);
+    cr_assert(list->tail->next == NULL);
+}
+
+
