95273084eeb51bd34c2c0b15f5af947e5b886165
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-02-01 18:34:06 +0300

    Test remove func

--- tests/test_list.c
+++ tests/test_list.c
@@ -5,6 +5,13 @@
 #include "cboxes/list.h"
 #include "cboxes/shallow.h"
 
+#define testing_IS_OK(expr, ...)                                               \
+    do {                                                                       \
+        cs_Status status = expr;                                               \
+        cr_assert(status == cs_OK, "CS call != cs_OK; status == %d\n",         \
+                  status __VA_OPT__(, ) __VA_ARGS__);                          \
+    } while (0)
+
 int randInt(int min, int max) { return min + rand() % (max + 1 - min); }
 
 int *generateArray(size_t length, int min, int max) {
@@ -33,9 +40,7 @@ void setup(void) {
     sdata.intList = generateList(10, 0, 20);
 }
 
-void teardown(void) {
-    cs_List_Free(sdata.intList);
-}
+void teardown(void) { cs_List_Free(sdata.intList); }
 
 TestSuite(test_list, .init = setup, .fini = teardown);
 
@@ -122,26 +127,29 @@ Test(test_list, List_DataAccess) {
 
     for (u64 i = 0; i < list->length; i++) {
         int *x = NULL;
-        cs_Status status = cs_List_Get(list, i, (void **)(&x));
-        cr_assert(status == cs_OK);
+        testing_IS_OK(cs_List_Get(list, i, (void **)(&x)));
         cr_assert(x != NULL);
     }
 
-    int *x; cs_Status status;
-    int indexes[3] = { -1, list->length, list->length + 1};
+    int *x;
+    cs_Status status;
+    int indexes[3] = {-1, list->length, list->length + 1};
 
     for (int i = 0; i < 3; i++) {
         x = NULL;
         status = CS_LIST_GET(list, indexes[i], x);
-        cr_assert(status == cs_INDEX_ERROR, "Tryed access '%d' index\n", indexes[i]);
+        cr_assert(status == cs_INDEX_ERROR, "Tryed access '%d' index\n",
+                  indexes[i]);
         cr_assert(x == NULL, "Tryed access '%d' index\n", indexes[i]);
     }
 }
 
 void testing_CheckListNodeRefs(cs_List *list) {
     CS_LIST_FOREACHN(list, i, n, {
-        if (i != 0) cr_assert(n->prev != NULL);
-        if (i != list->length -1) cr_assert(n->next != NULL);
+        if (i != 0)
+            cr_assert(n->prev != NULL);
+        if (i != list->length - 1)
+            cr_assert(n->next != NULL);
     });
 }
 
@@ -152,7 +160,7 @@ void testing_CheckListPopAtIndex(cs_List *list, unsigned long index) {
     CS_LIST_GET(list, index, &valueFromGet);
 
     int *valueFromPop = NULL;
-    cr_assert(cs_List_Pop(list, index, (void**)(&valueFromPop)) == cs_OK);
+    cr_assert(cs_List_Pop(list, index, (void **)(&valueFromPop)) == cs_OK);
 
     cr_assert(list->length == lengthBefore - 1);
     cr_assert(valueFromGet == valueFromPop);
@@ -175,6 +183,49 @@ Test(test_list, List_Deletion_Pop_Middle) {
 Test(test_list, List_Deletion_Pop_Last) {
     cs_List *list = sdata.intList;
     testing_CheckListPopAtIndex(list, list->length - 1);
+    testing_CheckListNodeRefs(list);
+}
+
+void testing_CheckListRemoveAtIndex(cs_List *list, unsigned long index) {
+
+    unsigned long lengthBefore = list->length;
+    int *itemBefore = NULL;
+    int *itemNext = NULL;
+
+    testing_IS_OK(CS_LIST_GET(list, index, &itemBefore));
+
+    testing_IS_OK(CS_LIST_GET(
+        list, index == list->length - 1 ? list->length - 2 : index + 1,
+        &itemNext));
+    testing_IS_OK(cs_List_Remove(list, index));
+
+    int *itemAfter = NULL;
+    testing_IS_OK(CS_LIST_GET(
+        list, index == list->length ? list->length - 1 : index, &itemAfter));
+
+    cr_assert(list->length == lengthBefore - 1);
+    cr_assert(*itemAfter != *itemBefore);
+    cr_assert(itemAfter != itemBefore);
+
+    cr_assert(*itemAfter == *itemNext);
+    cr_assert(itemAfter == itemNext);
+}
+
+Test(test_list, List_Deletion_Remove_First) {
+    cs_List *list = sdata.intList;
+    testing_CheckListRemoveAtIndex(list, 0);
+    testing_CheckListNodeRefs(list);
+}
+
+Test(test_list, List_Deletion_Remove_Middle) {
+    cs_List *list = sdata.intList;
+    testing_CheckListRemoveAtIndex(list, list->length / 2);
+    testing_CheckListNodeRefs(list);
+}
+
+Test(test_list, List_Deletion_Remove_Last) {
+    cs_List *list = sdata.intList;
+    testing_CheckListRemoveAtIndex(list, list->length - 1);
     testing_CheckListNodeRefs(list);
 }
 
