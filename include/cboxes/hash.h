d0e60e45b9bb28d020cd4993c49d3fbd43f4ee28
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-02-17 00:29:08 +0300

    Add hash functions

---
+++ include/cboxes/hash.h
@@ -0,0 +1,9 @@
+#ifndef HASH_H_
+#define HASH_H_
+
+#include "cboxes/types.h"
+
+// Stoled from http://www.cse.yorku.ca/~oz/hash.html
+u64 cs_lose_lose_hash(byte *str);
+
+#endif // HASH_H_
---
+++ src/hash.c
@@ -0,0 +1,13 @@
+#include "cboxes/hash.h"
+
+// Stoled from http://www.cse.yorku.ca/~oz/hash.html
+u64 cs_lose_lose_hash(byte *str) {
+    u64 hash = 0;
+    i32 c;
+
+    while ((c = *str++)) {
+        hash += c;
+    }
+
+    return hash;
+}
