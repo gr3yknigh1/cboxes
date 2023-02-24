7ae5f55accc7dcde93b647a909bfb634a43df009
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-02-24 14:52:30 +0300

    Add func decls

--- include/cboxes/hashmap.h
+++ include/cboxes/hashmap.h
@@ -2,8 +2,10 @@
 #define HASHMAP_H_
 
 #include "cboxes/list.h"
-#include "cboxes/type.h"
 #include "cboxes/numtypes.h"
+#include "cboxes/status.h"
+#include "cboxes/string.h"
+#include "cboxes/type.h"
 
 typedef struct cs_Hashmap {
     cs_List *buckets;
@@ -13,6 +15,15 @@ typedef struct cs_Hashmap {
     u64 capacity;
 } cs_Hashmap;
 
+cs_Hashmap *cs_Hashmap_New(cs_Type *type, u64 capacity);
+cs_Hashmap *cs_Hashmap_NewD(size_t size, u64 capacity);
 
+void cs_Hashmap_Push(cs_Hashmap *hashmap, cstr key, void *value);
+cs_Status cs_Hashmap_Set(cs_Hashmap *hashmap, cstr key, void *value);
+cs_Status cs_Hashmap_Get(cs_Hashmap *hashmap, cstr key, void **out);
+cs_Status cs_Hashmap_Pop(cs_Hashmap *hashmap, cstr key, void **out);
+cs_Status cs_Hashmap_Remove(cs_Hashmap *hashmap, cstr key);
+
+void cs_Hashmap_Free(void *ptr);
 
 #endif // HASHMAP_H_
