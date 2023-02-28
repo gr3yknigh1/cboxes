2e5b3e1c98071ac2cd3685bb17b3a4ac764989de
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-02-28 13:33:29 +0300

    Add `cs_Pair`

--- include/cboxes/pair.h
+++ include/cboxes/pair.h
@@ -2,10 +2,17 @@
 #define PAIR_H_
 
 #include "cboxes/string.h"
+#include "cboxes/type.h"
 
 typedef struct cs_Pair {
-    cstr key;
+    cs_String *key;
     void *value;
+    cs_Type *type;
 } cs_Pair;
+
+cs_Pair *cs_Pair_New(cstr key, void *value, cs_Type *type);
+
+void *cs_Pair_Copy(void *dest, const void *src, size_t count);
+void cs_Pair_Free(void *ptr);
 
 #endif // PAIR_H_
---
+++ src/pair.c
@@ -0,0 +1,30 @@
+#include "cboxes/pair.h"
+#include "cboxes/string.h"
+#include "cboxes/type.h"
+
+cs_Pair *cs_Pair_New(cstr key, void *value, cs_Type *type) {
+    cs_Pair *pair = malloc(sizeof(cs_Pair));
+    *pair = (cs_Pair){
+        .key = cs_String_New(key),
+        .value = cs_Type_StoreValue(type, value),
+        .type = cs_Type_NewC(type),
+    };
+    return pair;
+}
+
+void *cs_Pair_Copy(void *dest, const void *src, size_t count) {
+    for (size_t i = 0; i < count; i++) {
+        ((byte *)dest)[i] = ((byte *)src)[i];
+    }
+    return dest;
+}
+
+void cs_Pair_Free(void *ptr) {
+    cs_Pair *pair = (cs_Pair *)ptr;
+
+    pair->type->free(pair->value);
+
+    cs_String_Free(pair->key);
+    cs_Type_Free(pair->type);
+    free(ptr);
+}
