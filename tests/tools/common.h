f047572679d351190b4f2f9e24d8d48bc0aa894e
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-03-05 20:13:03 +0300

    Add `randStr` and `randChar` funcs in `tools/common.h`

--- tests/tools/common.h
+++ tests/tools/common.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 int randInt(int min, int max) { return min + rand() % (max + 1 - min); }
 
@@ -9,4 +10,18 @@ int *generateArray(size_t length, int min, int max) {
     }
     return array;
 }
+
+char randChar() {
+    return randInt(33, 126);
+}
+
+const char *randStr(uint64_t min, uint64_t max) {
+    size_t size = randInt(min, max);
+    char *string = malloc(size);
+    for (size_t i = 0; i < size; i++) {
+        string[i] = randChar();
+    }
+    return string;
+}
+
 
