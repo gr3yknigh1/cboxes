58bc62c3210bbcabab910c4f68ca4de5751a6da3
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-03-07 15:28:27 +0300

    Switch to fix-sized num types in `numtypes.h`

--- include/cboxes/numtypes.h
+++ include/cboxes/numtypes.h
@@ -1,15 +1,24 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-typedef unsigned long u64;
-typedef unsigned int u32;
-typedef unsigned short u16;
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
-typedef signed long i64;
-typedef signed int i32;
-typedef signed short i16;
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
 
 typedef double f64;
 typedef float f32;
+
+typedef size_t usize;
+typedef ssize_t isize;
 
 #endif // TYPES_H_
