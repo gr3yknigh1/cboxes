742c1f2f5603d951d7717918bded9b9802a891dd
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-06-25 12:37:36 +0300

    Add first test unittest for darray

---
+++ tests/test_darray.c
@@ -0,0 +1,19 @@
+#include "ccheck.h"
+
+#include "cboxes/darray.h"
+
+CC_TEST_SUITE(DARR);
+
+CC_TEST_CASE(DATA_SIZE) {
+    cs_darray_t arr = {
+        .data = NULL,
+        .len = 0,
+        .cap = 10,
+        .type = CS_TYPE_I32,
+    };
+
+    size_t testing_size = CS_DARRAY_DATA_SIZE(&arr);
+    size_t expected_size = 10 * arr.type->size;
+
+    ASSERT(testing_size == expected_size);
+}
